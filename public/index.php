14b553c6c5f0ac34573e2a8e1f8ddfcac0803884
michalsniezko <michalsniezko@imtechnology.co.uk>
2017-09-20 13:59:51 +0200

    Move autoloading to composer.json using psr-4 autoload

--- public/index.php
+++ public/index.php
@@ -2,17 +2,6 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-spl_autoload_register(function($class){
-    $root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if(is_readable($file))
-    {
-        /** @noinspection PhpIncludeInspection */
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
-
-
 $router = new \Core\Router();
 
 $router->add('', ['controller'=>'Home', 'action'=>'index']);
@@ -20,25 +9,6 @@ $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
 $router->add('admin/{controller}/{action}', ['namespace' => 'Admin']);
-
-/* Display the routing table:
-echo '<pre>';
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
-
-$url = $_SERVER['QUERY_STRING'];
-
-if($router->match($url))
-{
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-}
-else
-{
-    echo "No route found for URL: $url
-}
-*/
 
 //$router->printRoutingTable();
 $router->dispatch($_SERVER['QUERY_STRING']);
